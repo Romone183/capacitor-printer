9bfca195af98fcb5f337bead586204fcb35aad14
Romain Vincent <rovincent@yahoo.fr>
2023-08-30 17:40:36 +0200

    Android config

--- android/src/main/java/com/romone183/capacitor/printer/PrinterPlugin.java
+++ android/src/main/java/com/romone183/capacitor/printer/PrinterPlugin.java
@@ -1,5 +1,15 @@
 package com.romone183.capacitor.printer;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.print.PrintAttributes;
+import android.print.PrintDocumentAdapter;
+import android.print.PrintJob;
+import android.webkit.WebView;
+
+import androidx.annotation.NonNull;
+
+import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
@@ -9,14 +19,62 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 @CapacitorPlugin(name = "Printer")
 public class PrinterPlugin extends Plugin {
 
-    private Printer implementation = new Printer();
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void check(PluginCall call) {
+        String item = call.getString("item");
 
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
+        boolean printable = canPrintItem(item);
+
+        JSObject result = new JSObject();
+        result.put("printable", printable);
+
+        call.resolve(result);
     }
+
+    @PluginMethod
+    public void types(PluginCall call) {
+        JSArray utis = getPrintableTypes();
+
+        JSObject result = new JSObject();
+        result.put("types", utis);
+
+        call.resolve(result);
+    }
+
+    @PluginMethod
+    public void print(PluginCall call) {
+        String content = call.getString("content");
+        JSObject settings = call.getObject("settings");
+
+        printDocument(content, settings, call);
+    }
+
+    private boolean canPrintItem(String item) {
+        // Replace this with logic to check if item can be printed
+        return false;
+    }
+
+    private JSArray getPrintableTypes() {
+        // Replace this with logic to get printable types
+        // For example: String[] types = PrintManager.getPrintableTypes();
+        // Convert types array to JSArray and return
+        return new JSArray();
+    }
+
+    private void printDocument(String content, JSObject settings, PluginCall call) {
+
+        PrintManager pm = new PrintManager(this.getContext());
+        pm.print(content, settings, this.getBridge().getWebView(), (boolean completed)-> sendPluginResult(call, completed) );
+
+
+    }
+    private void sendPluginResult(@NonNull PluginCall call, boolean value) {
+
+        JSObject result = new JSObject();
+        result.put("status", value);
+
+        call.resolve(result);
+    }
+
 }
--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,4 @@
 export interface PrinterPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
+  print(options: { content: string }): Promise<void>;
 }
